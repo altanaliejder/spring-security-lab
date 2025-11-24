3c1c5dfcec690c46ce2e7dc8868db3f334adfa53
altanaliejder <altanalie@gmail.com>
2025-11-25 01:22:54 +0300

    bugfix and improvements

--- src/main/java/com/ejdev/securitylab/auth/dto/AuthRequest.java
+++ src/main/java/com/ejdev/securitylab/auth/dto/AuthRequest.java
@@ -2,13 +2,14 @@ package com.ejdev.securitylab.auth.dto;
 
 import com.ejdev.securitylab.auth.strategy.AuthStrategy;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record AuthRequest (
         @NotBlank
         String username,
         @NotBlank
         String password,
-        @NotBlank
+        @NotNull
         AuthStrategy strategy
         ){
 }
